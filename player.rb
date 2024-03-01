b77b2813ed548f7ceb2153b2bd96b94f721b3edd
Cedric.gauthier <cidgaut@gmail.com>
2024-03-01 12:28:09 -0500

    created player class displays lives remaining

--- player.rb
+++ player.rb
@@ -1,1 +1,27 @@
 # player class file that holds player name and keeps track of lives
+
+class Player
+  attr_accessor :name, :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def lose_life
+    @lives -=1
+  end
+
+  def player_score
+    "#{name}: #{lives} lives"
+  end
+end
+
+player1 = Player.new("Player 1")
+puts player1.player_score
+player1.lose_life
+puts player1.player_score
+player1.lose_life
+puts player1.player_score
+player1.lose_life
+puts player1.player_score
